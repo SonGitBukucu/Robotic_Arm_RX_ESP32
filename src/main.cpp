d237c38e1fcb3566e6633fb682b2e77561b5ebe7
SonGitBukucu <makaya2620@gmail.com>
2026-01-16 12:33:20 +0300

    add servo library and critical variables

--- src/main.cpp
+++ src/main.cpp
@@ -3,36 +3,60 @@
 #include <RF24.h>
 #include <nRF24L01.h>
 #include <SD.h>
+#include <ESP32Servo.h>
 
+#define servo1Pin
+#define servo2Pin
+#define servo3Pin
+#define servo4Pin
+#define servo5Pin
+#define servo6Pin
+#define servo7Pin
+#define servo8Pin
 
-TaskHandle_t task1;
-TaskHandle_t task2;
+#define playbackArti
+#define playbackEksi
+#define kolModu
+
+TaskHandle_t iletisim;
+TaskHandle_t sdKart;
 
 int sayac0 = 0;
 int sayac1 = 0;
 
-void task1code(void * parameter);
-void task2code(void * parameter);
+void iletisimCode(void * parameter);
+void sdKartCode(void * parameter);
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6;
+Servo servo7;
+Servo servo8;
+
+
 
 void setup() {
   Serial.begin(115200);
 
   xTaskCreatePinnedToCore(
-      task1code, /* Function to implement the task */
-      "Task1", /* Name of the task */
-      10000,  /* Stack size in words */
-      NULL,  /* Task input parameter */
-      1,  /* Priority of the task */
-      &task1,  /* Task handle. */
-      1); /* Core where the task should run */
-  delay(500);
-  xTaskCreatePinnedToCore(
-    task2code, /* Function to implement the task */
+    iletisimCode, /* Function to implement the task */
     "Task1", /* Name of the task */
     10000,  /* Stack size in words */
     NULL,  /* Task input parameter */
     1,  /* Priority of the task */
-    &task2,  /* Task handle. */
+    &iletisim,  /* Task handle. */
+    1); /* Core where the task should run */
+  delay(500);
+  xTaskCreatePinnedToCore(
+    sdKartCode, /* Function to implement the task */
+    "Task1", /* Name of the task */
+    10000,  /* Stack size in words */
+    NULL,  /* Task input parameter */
+    1,  /* Priority of the task */
+    &sdKart,  /* Task handle. */
     0); /* Core where the task should run */
 }
 
@@ -40,7 +64,7 @@ void loop() {
 
 }
 
-void task1code( void * parameter) {
+void iletisimCode( void * parameter) {
   Serial.println(xPortGetCoreID());
   for(;;) {
     Serial.println(sayac0++);
@@ -48,7 +72,7 @@ void task1code( void * parameter) {
   }
 }
 
-void task2code( void * parameter) {
+void sdKartCode( void * parameter) {
   Serial.println(xPortGetCoreID());
   for(;;) {
     Serial.println(sayac1++);
